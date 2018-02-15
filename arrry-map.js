9eddc753431666bd22a9124cc7d381278134104b
Peter Horton <phorton@softstackfactory.org>
2018-02-15 04:01:45 +0000

    array ver 2

---
+++ arrry-map.js
@@ -0,0 +1,22 @@
+const Animals = [
+    { name: "Fluffykins", species: "rabbit" },
+    { name: "Caro", species: "dog" },
+    { name: "Hamilton", species: "dog" },
+    { name: "Harold", species: "fish" },
+    { name: "Ursula", species: "cat" },
+    { name: "Jimmy", species: "fish" }
+]
+
+const animalMap = function(array) {
+    let animalMapArray = []
+    for (let i = 0; i < array.length; i++) {
+        animalMapArray.push(array[i].name);
+
+    }
+    return animalMapArray
+
+}
+
+
+let animalNames = animalMap(Animals)
+console.log(animalNames)
